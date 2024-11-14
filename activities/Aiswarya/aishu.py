c6afae032e71bfe8614efa83148059b8af2953a8
masterbrain09 <aiswaryagec@gmail.com>
2024-11-14 13:57:40 +0530

    Aiswarya's code (#33)

---
+++ activities/Aiswarya/aishu.py
@@ -0,0 +1,26 @@
+#wap to create quiz
+print("Starting position of an array is")
+print("a) 1")
+print("b) 0")
+print("c) -1")
+i=input("enter answer")
+if i=="b":
+    print("CORRECT")
+else:
+    print("INCORRECT")
+print("Git is a")
+print("a) website")
+print("b) version control system")
+i=input("enter answer")
+if i=="b":
+    print("CORRECT")
+else:
+    print("INCORRECT")
+print("Do you need github to use git")
+print("a) yes")
+print("b) no")
+i=input("enter answer")
+if i=="b":
+    print("CORRECT")
+else:
+    print("INCORRECT")
